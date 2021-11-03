b3f59d98829bcd870a8c58b8c8c1aa7fdb1989fa
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-03 19:18:50 +0100

    store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootStore } from "./Store";
+import { GetPokemon } from "./actions/PokemonActions";
 
 function App() {
-  return <div className="App"></div>;
+  const dispatch = useDispatch();
+  const [pokemonName, setPokemonName] = useState("");
+  const pokemonState = useSelector((state: RootStore) => state.pokemon);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setPokemonName(event.target.value);
+  const handleSubmit = () => dispatch(GetPokemon(pokemonName));
+
+  console.log("pokemon state:", pokemonState);
+  return (
+    <div className="App">
+      <input type="text" onChange={handleChange} />
+      <button onClick={handleSubmit}>Search</button>
+      {pokemonState.pokemon && (
+        <div>
+          <img src={pokemonState.pokemon.sprites.front_default} alt="" />
+          {pokemonState.pokemon.abilities.map((ability) => {
+            return <p>{ability.ability.name}</p>;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
